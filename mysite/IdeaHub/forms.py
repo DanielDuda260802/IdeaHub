59fbaa097e6474c26d0945b422b808b1778a04c2
daniel <dudadaniel2608@gmail.com>
2024-02-06 19:14:23 +0100

    Dodana autentifikacija za uređivanje, brisanje objava (ovisno o ID-u usera i autora), Popravljena logika postavljanja autora automatski na prijavljenog korisnika koji kreira post

--- mysite/IdeaHub/forms.py
+++ mysite/IdeaHub/forms.py
@@ -9,13 +9,16 @@ for item in choices:
 class PostForm(forms.ModelForm):
     class Meta:
         model = Post
-        fields = ('title', 'title_tag', 'category', 'author', 'body' )
+        fields = ('title', 'title_tag', 'author', 'category', 'body' )
 
         widgets = {
             'title': forms.TextInput(attrs={'class': 'form-control'}), # form-control --> Bootstrap item
             'title_tag': forms.TextInput(attrs={'class': 'form-control'}),
+            # 'value' --> whatever our Javascript has told it to be
+            # 'id':'user' --> allows writing JavaScript files
+            # 'type':'hidden' --> make TextInput hidden so users can't change the post's author
+            'author': forms.TextInput(attrs={'class': 'form-control', 'value':'', 'id':'user', 'type':'hidden'}),  
             'category': forms.Select(choices=choice_list, attrs={'class': 'form-control'}),
-            'author': forms.Select(attrs={'class': 'form-control'}),
             'body': forms.Textarea(attrs={'class': 'form-control'})
         }
 
