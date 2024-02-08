59691c1649f4815a547000006bd317c4a10bbd0a
daniel <dudadaniel2608@gmail.com>
2024-02-08 19:16:44 +0100

    Implementirane dodatne opcije za edit profile, u idea_details dodatan prikaz podataka o profilu korisnika koji je objavio clanak

--- mysite/IdeaHub/admin.py
+++ mysite/IdeaHub/admin.py
@@ -2,5 +2,5 @@ from django.contrib import admin
 from .models import *
 
 # Register your models here.
-models_list = [Post, Category]
+models_list = [Post, Category, Profile]
 admin.site.register(models_list)
--- mysite/IdeaHub/models.py
+++ mysite/IdeaHub/models.py
@@ -35,3 +35,15 @@ class Category(models.Model):
     
     def get_absolute_url(self):
         return reverse('IdeaHub:homepage')
+    
+class Profile(models.Model):
+    user = models.OneToOneField(User, null=True, on_delete=models.CASCADE)
+    bio = models.TextField()
+    profile_picture = models.ImageField(null=True, blank=True, upload_to="images/profile/")
+    website_url = models.CharField(max_length=255, null=True, blank=True)
+    facebook_url = models.CharField(max_length=255, null=True, blank=True)
+    instagram_url = models.CharField(max_length=255, null=True, blank=True)
+    github_url = models.CharField(max_length=255, null=True, blank=True)
+
+    def __str__(self):
+        return str(self.user)
