87430360d6c14ef961760534db18e43c8c132b8e
daniel <dudadaniel2608@gmail.com>
2024-02-15 19:51:48 +0100

    Uređen dizajn homepage-a, forme za dodavanje nove objave

--- mysite/IdeaHub/forms.py
+++ mysite/IdeaHub/forms.py
@@ -3,9 +3,7 @@ from .models import Post, Category, Comment
 from django.utils import timezone
 
 choices = Category.objects.all().values_list('name', 'name')
-choice_list = []
-for item in choices:
-    choice_list.append(item)
+choice_list = [(choice[0], choice[1].title()) for choice in choices]
 
 class PostForm(forms.ModelForm):
     class Meta:
@@ -13,14 +11,15 @@ class PostForm(forms.ModelForm):
         fields = ('title', 'title_tag', 'author', 'category', 'body', 'header_image' )
 
         widgets = {
-            'title': forms.TextInput(attrs={'class': 'form-control'}), # form-control --> Bootstrap item
-            'title_tag': forms.TextInput(attrs={'class': 'form-control'}),
+            'title': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Enter post title here'}), # form-control --> Bootstrap item
+            'title_tag': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Enter title tag here'}),
             # 'value' --> whatever our Javascript has told it to be
             # 'id':'user' --> allows writing JavaScript files
             # 'type':'hidden' --> make TextInput hidden so users can't change the post's author
             'author': forms.TextInput(attrs={'class': 'form-control', 'value':'', 'id':'user', 'type':'hidden'}),  
-            'category': forms.Select(choices=choice_list, attrs={'class': 'form-control'}),
+            'category': forms.Select(choices=choice_list, attrs={'class': 'form-control custom-select'}),
             'body': forms.HiddenInput(),
+            'header_image': forms.FileInput(attrs={'class': 'form-control-file'})
         }
 
 class EditForm(forms.ModelForm):
--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -39,7 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'IdeaHub.apps.IdeahubConfig',
-    'LoginApp.apps.LoginappConfig'
+    'LoginApp.apps.LoginappConfig',
+    'widget_tweaks',
 ]
 
 MIDDLEWARE = [
